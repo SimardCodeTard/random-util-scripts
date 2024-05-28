f432ff38e4b8705e30fe12b8a96e976b1cc32aa1
Gerard2par2 <floogles71@gmail.com>
2024-05-28 19:08:21 +0200

    python/randum: add min param

--- python/randnum.py
+++ python/randnum.py
@@ -3,4 +3,5 @@ import sys
 
 # Generate a random number between 0 and the value passed as first parameter
 max = int(sys.argv[1]) if len(sys.argv) > 1 else 100
-print('Your rolled {}'.format(randint(0, max)))
+min = int(sys.argv[2]) if len(sys.argv) > 2 else 0
+print('Your rolled {}'.format(randint(min, max)))
