f08814b633f08c7d453a25ee57d2088c1d5f7f7b
Gerard2par2 <floogles71@gmail.com>
2024-05-28 20:28:41 +0200

    update: script/copiedFileRemover: removed debug logs, simplified marker checking & removal, added type hinting to removeCopiedFiles function & improved readability & efficiency of file handling (gpt clearly did all the work)

--- python/copiedFileRemover.py
+++ python/copiedFileRemover.py
@@ -3,38 +3,30 @@ import sys
 import re
 from typing import List
 
-copiedFileMarkers: List[str] = []
-
-def removeCopiedFiles(directoryPath: str):
+def removeCopiedFiles(directoryPath: str, copiedFileMarkers: List[str]):
     print('-' * 50)
     print("Checking directory: ", directoryPath)
     nestedDirectories: List[str] = []
     files: List[str] = os.listdir(directoryPath)
+    
     for file in files:
         full_path = os.path.join(directoryPath, file)
-        print(f'Checking file "{file}"')
         if os.path.isdir(full_path):
-            print("Found directory: ", full_path)
             nestedDirectories.append(full_path)
-            continue
-        for marker in copiedFileMarkers:
-            print(f'Checking marker "{marker}" for file "{file}"')
-            if re.search(marker, file):
-                markerCount = len(re.findall(marker, file))
-                for _ in range(markerCount):
+        else:
+            for marker in copiedFileMarkers:
+                if re.search(marker, file):
                     original_file = re.sub(marker, '', file)
-                print(f'Found marker "{marker}" {markerCount} times in file "{file}"')
-                print(f'Assumed original file name: "{original_file}"')
-                if original_file in files:
-                    print(f'Found original file "{original_file}" in the same directory')
-                    try:
-                        os.remove(full_path)
-                        print(f'Removed file "{full_path}"')
-                    except Exception as e:
-                        print(f'Error removing file "{full_path}": {e}')
+                    if original_file in files:
+                        try:
+                            os.remove(full_path)
+                            print(f'Removed file "{full_path}"')
+                        except OSError as e:
+                            print(f'Error removing file "{full_path}": {e}')
+                    break
     
     for directory in nestedDirectories:
-        removeCopiedFiles(directory)
+        removeCopiedFiles(directory, copiedFileMarkers)
 
 # Check if the user has passed the path
 if len(sys.argv) > 1:
@@ -45,4 +37,4 @@ else:
 # Define copied file markers with regex patterns
 copiedFileMarkers = [r' - Copie', r' - Copy', r'\(\d+\)']
 
-removeCopiedFiles(path)
+removeCopiedFiles(path, copiedFileMarkers)
